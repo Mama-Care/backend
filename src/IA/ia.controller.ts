6ae06884d662effe5b439ea7ceead2440ffc89d4
brenno <martiniano.brenno@gmail.com>
2024-11-24 02:34:34 -0300

    fix: update controller and service ia

--- src/IA/ia.controller.ts
+++ src/IA/ia.controller.ts
@@ -5,14 +5,14 @@ import { IaService } from './ia.service';
 export class IaController {
   constructor(private readonly iaService: IaService) {}
 
-  @Post('sendQuestion')
-  async ask(@Body('question') question: string) {
-    return this.iaService.sendData(question);
-  }
+  @Post('sendQuestion/:id')
+  async ask(@Param('id') id: string, @Body('question') question: string) {
+  return this.iaService.sendData(id, question);
+}
 
-  @Get('user/:uid')
-  async getUserData(@Param('uid') uid: string): Promise<any> {
-    const data = await this.iaService.getUserData(uid);
-    return { conteudo: data }; // Retorna os dados em formato JSON
+  @Get('user/:id') 
+  async getUserData(@Param('id') id: string): Promise<any> {
+    const data = await this.iaService.getUserData(id);
+    return { conteudo: data };
   }
 }
